a538e2c3a542bcf3a56eccd1d46d1efe1385b6bb
Manthan Dev <manthan.dev1024@gmail.com>
2020-09-30 21:30:27 +0530

    setup changes

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const url = config.mongoUrl;
+const connect = mongoose.connect(url);
+
+connect.then(() => {
+    console.log("Connected to server");
+}, (err) => { console.log(err); });
 
 var app = express();
 
@@ -16,7 +23,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
